7469c51504b808124628d2865a51f2d1c491dd7e
eaaxsnn <easonisgey@gmail.com>
2023-05-25 12:17:37 +0800

    yesss

--- hello.py
+++ hello.py
@@ -1,3 +1,18 @@
-print("touch me")
-print("I love balls more than u do")
-# ni mama
+def closest_object(roundTrips):
+    #Perform the parsing of roundTrips input here
+    total = []
+    closestObject = []
+    speed_list = roundTrips.split(',')
+    if len(speed_list) != 5:
+            closestObject = [-1,-1]
+    
+    else:
+        for i,x in enumerate(speed_list):
+            dist = (float(speed_list[i]) * 344 ) / 2
+            total.append(dist)
+        
+            closestObject = [total.index(min(total)), min(total)]    
+
+    
+    
+    return closestObject#Do not remove this line

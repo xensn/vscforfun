45168fd0e5c0e7b275f8f2073da14af68533d580
eaaxsnn <easonisgey@gmail.com>
2023-05-25 12:21:04 +0800

    hello

--- yes.py
+++ yes.py
@@ -1,1 +1,25 @@
-print(('Invalid', 'Valid')[all(map(lambda f: f(input('Enter a password: ')), [(lambda s: len(s) >= 8), str.islower, str.isupper, str.isdigit]))] + ' password')
+def find_closest_object():
+    timings = input("Enter the round trip times (seconds) separated by commas: ").split(",")
+    timings = [float(time.strip()) for time in timings]
+
+    if len(timings) < 5:
+        print("Error: Please enter at least 5 round trip times.")
+        return [-1, -1]
+
+    closest_distance = float('inf')
+    closest_index = -1
+
+    for i, time in enumerate(timings):
+        distance = time * 344 / 2
+        if distance < closest_distance:
+            closest_distance = distance
+            closest_index = i
+
+    print("Closest object index:", closest_index)
+    print("Distance to closest object:", closest_distance, "m")
+
+    return [closest_index, closest_distance]
+
+
+# Call the function
+find_closest_object()

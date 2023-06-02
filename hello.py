248e2d6d568fcd0f71ffa91ca2e106be0ac7d43d
eaaxsnn <easonisgey@gmail.com>
2023-06-03 00:25:14 +0800

    Dhava

--- hello.py
+++ hello.py
@@ -1,18 +1,38 @@
-def closest_object(roundTrips):
-    #Perform the parsing of roundTrips input here
-    total = []
-    closestObject = []
-    speed_list = roundTrips.split(',')
-    if len(speed_list) != 5:
-            closestObject = [-1,-1]
+import math
+
+mark_list = []
+
+# Ask user for Number of student and marks
+student = int(input("Number of students: "))
+
+
+# Putting List of student into Dict
+marks_dic = {}
+for i in range(1, student+1):
+    student_marks = str(input("Enter marks a,b,c,d: "))
+    marks_dic[i] = student_marks.split(',')
     
-    else:
-        for i,x in enumerate(speed_list):
-            dist = (float(speed_list[i]) * 344 ) / 2
-            total.append(dist)
-        
-            closestObject = [total.index(min(total)), min(total)]    
+total = 0
+# Total marks for each student
+for i in range(1,student+1):
+    marks_list = marks_dic[i]
+    x = (int(marks_list[0])+int(marks_list[1])+int(marks_list[2])+int(marks_list[3]))
+    print(f"Student {i}:   {x} marks")
+    total += x
+    
+
+# Average for each test
+mark_list1 = marks_dic[1]
+mark_list2 = marks_dic[2]
+mark_list3 = marks_dic[3]
+print(f"Average for Test 1:   {((int(mark_list1[0])+int(mark_list2[0])+int(mark_list3[0])))//4} marks")
+print(f"Average for Test 2:   {((int(mark_list1[1])+int(mark_list2[1])+int(mark_list3[1])))//4} marks")
+print(f"Average for Test 3:   {((int(mark_list1[2])+int(mark_list2[2])+int(mark_list3[2])))//4} marks")
+print(f"Average for Test 4:   {((int(mark_list1[3])+int(mark_list2[3])+int(mark_list3[3])))//4} marks")
+
+
 
     
-    
-    return closestObject#Do not remove this line
+
+average = math.ceil(total / student)
+print(f'Average score per student:   {average} marks')
